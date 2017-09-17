b6afaef3c18b5c9f5f7abe312d1ae6e1966a440a
Dan Fickle <newtab@gmail.com>
2017-09-17 15:57:07 +1000

    Initial commit. Everything working!

---
+++ src/main/java/com/openhtmltopdf/projects/resume/Entity.java
@@ -0,0 +1,102 @@
+package com.openhtmltopdf.projects.resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+public class Entity {
+	public static class Name {
+		@JsonProperty public String first;
+		@JsonProperty public String last;
+	}
+	
+	public static class Birth {
+		@JsonProperty public Integer year;
+		@JsonProperty public String location;
+	}
+	
+	public static class Experience {
+		@JsonProperty public String company;
+		@JsonProperty public String position;
+		@JsonProperty public String timeperiod;
+		@JsonProperty public String description;
+	}
+	
+	public static class Education {
+		@JsonProperty public String degree;
+		@JsonProperty public String timeperiod;
+		@JsonProperty public String description;
+	}
+	
+	public static class Skill {
+		@JsonProperty public String name;
+		@JsonProperty public int level;
+	}
+	
+	public static class Contact {
+		@JsonProperty public String email;
+		@JsonProperty public String phone;
+		@JsonProperty public String street;
+		@JsonProperty public String city;
+		@JsonProperty public String website;
+		@JsonProperty public String github;
+	}
+	
+	public static class PersonEntity {
+		@JsonProperty
+		public ResumeEntity person;
+		
+		@JsonProperty
+		public String lang;
+	}
+	
+	public static class ResumeEntity {
+		@JsonProperty
+		public Name name;
+		
+		@JsonProperty
+		public String position;
+		
+		@JsonProperty
+		public Birth birth;
+		
+		@JsonProperty
+		@JsonDeserialize(as=ArrayList.class, contentAs=Experience.class)
+		public List<Experience> experience;
+		
+		@JsonProperty
+		@JsonDeserialize(as=ArrayList.class, contentAs=Education.class)
+		public List<Education> education;
+		
+		@JsonProperty
+		@JsonDeserialize(as=ArrayList.class, contentAs=Skill.class)
+		public List<Skill> skills;
+		
+		@JsonProperty
+		public String skillDescription;
+		
+		@JsonProperty
+		public Contact contact;
+	}
+
+	public static class Headings {
+		@JsonProperty
+		public String contact;
+		
+		@JsonProperty
+		public String experience;
+		
+		@JsonProperty
+		public String education;
+		
+		@JsonProperty
+		public String skills;
+	}
+	
+	public static class LangEntity {
+		@JsonProperty
+		public Headings headings;
+	}
+}
---
+++ src/main/java/com/openhtmltopdf/projects/resume/Generator.java
@@ -0,0 +1,283 @@
+package com.openhtmltopdf.projects.resume;
+
+import static spark.Spark.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.EnumMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;import com.openhtmltopdf.extend.FSUriResolver;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import com.openhtmltopdf.util.XRLog;
+
+public class Generator {
+	
+	// We can get rid of this once a fix for issue-#125 is released with RC12 of OpenHTMLtoPDF.
+	public static class UriResolver implements FSUriResolver {
+		/**
+		 * Resolves the URI; if absolute, leaves as is, if relative, returns an
+		 * absolute URI based on the baseUrl for the agent.
+		 *
+		 * @param uri
+		 *            A URI, possibly relative.
+		 *
+		 * @return A URI as String, resolved, or null if there was an exception
+		 *         (for example if the URI is malformed).
+		 */
+		@Override
+		public String resolveURI(String baseUri, String uri) {
+			if (uri == null || uri.isEmpty())
+				return null;
+			
+			try {
+				URI possiblyRelative = new URI(uri);
+				
+				if (possiblyRelative.isAbsolute()) {
+					return possiblyRelative.toString();
+				} else {
+				    if(baseUri.startsWith("jar")) {
+				    	// Fix for OpenHTMLtoPDF issue-#125, URI class doesn't resolve jar: scheme urls and so returns only
+				    	// the relative part on calling base.resolve(relative) so we use the URL class instead which does
+				    	// understand jar: scheme urls.
+				    	URL base = new URL(baseUri);
+				        URL absolute = new URL(base, uri);
+				        return absolute.toString();
+				    } else {
+						URI base = new URI(baseUri);
+						URI absolute = base.resolve(uri);
+						return absolute.toString();				    	
+				    }
+				}
+			} catch (URISyntaxException e) {
+				XRLog.exception("When trying to load uri(" + uri + ") with base URI(" + baseUri + "), one or both were invalid URIs.", e);
+				return null;
+			} catch (MalformedURLException e) {
+				XRLog.exception("When trying to load uri(" + uri + ") with base jar scheme URI(" + baseUri + "), one or both were invalid URIs.", e);
+				return null;
+			}
+		}
+	}
+	
+	private static enum Editor {
+		RAW,
+		ANGULARJS1X;
+	}
+	
+	private static enum Languages {
+		ENGLISH("en"),
+		GERMAN("de"),
+		FRENCH("fr");
+		
+		public final String slug;
+		
+		private Languages(String slug) {
+			this.slug = slug;
+		}
+	}
+	
+	private static enum Template {
+		MATERIAL_BLUE("material-blue");
+		
+		public final String slug;
+		
+		private Template(String slug) {
+			this.slug = slug;
+		}
+	}
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Generator.class);
+	private static final SecureRandom RANDOM = new SecureRandom();
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+	private static final TemplateEngine THYMELEAF = new TemplateEngine();
+	private static final Map<String, String> langs = new ConcurrentHashMap<>();
+	private static final Map<String, String> templates = new ConcurrentHashMap<>();
+	private static final Map<Editor, String> editorPages = new EnumMap<>(Editor.class);
+	
+	private static void loadLang(Languages lang) {
+		try {
+			langs.put(lang.slug, IOUtils.toString(Generator.class.getResource("/languages/" + lang.slug + ".json"), StandardCharsets.UTF_8));
+			LOGGER.info("Loaded language file: {}", lang);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+	private static void loadTemplate(Template t) {
+		try {
+			templates.put(t.slug, IOUtils.toString(Generator.class.getResource("/" + t.slug + ".xhtml"), StandardCharsets.UTF_8));
+			LOGGER.info("Loaded template: {}", t);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	private static void loadEditorPage(Editor e) {
+		try {
+			editorPages.put(e, IOUtils.toString(Generator.class.getResource("/pages/" + e.toString().toLowerCase(Locale.US) + ".xhtml"), StandardCharsets.UTF_8)); 
+			LOGGER.info("Loaded editor file: {}", e);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	static {
+		MAPPER.enable(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES);
+		MAPPER.enable(JsonParser.Feature.ALLOW_SINGLE_QUOTES);
+		MAPPER.enable(JsonParser.Feature.ALLOW_MISSING_VALUES);
+		MAPPER.enable(JsonParser.Feature.ALLOW_COMMENTS);
+		MAPPER.enable(JsonParser.Feature.ALLOW_TRAILING_COMMA);
+		
+		Stream.of(Editor.values()).forEach(Generator::loadEditorPage);
+		Stream.of(Languages.values()).forEach(Generator::loadLang);
+		Stream.of(Template.values()).forEach(Generator::loadTemplate);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Class.forName("org.h2.Driver");
+		
+		String defaultResume = IOUtils.toString(Generator.class.getResource("/default-resume.json"), "UTF-8");
+		
+		try(Connection conn = DriverManager.getConnection("jdbc:h2:mem:resume;DB_CLOSE_DELAY=-1", "sa", "")) {
+			String sql = 
+				"CREATE TABLE IF NOT EXISTS resumes ( " +
+				"id BIGINT IDENTITY PRIMARY KEY, json VARCHAR(10000), token VARCHAR(50), ts TIMESTAMP, template VARCHAR(64) )";
+			
+			conn.createStatement().executeUpdate(sql);
+		}
+		
+		post("/upload", (req, res) -> {
+			try(Connection conn = DriverManager.getConnection("jdbc:h2:mem:resume;DB_CLOSE_DELAY=-1", "sa", "")) {
+				String json = req.queryParams("resumejson");
+				
+				// Check we support the template.
+				if (!Stream.of(Template.values()).anyMatch(t -> t.slug.equals(req.queryParams("templateslug")))) {
+					LOGGER.warn("Tried to use non-existant template: {}", req.queryParams("templateslug"));
+					halt(404);
+				}
+				
+				// Validate the json by trying to map it.
+				Entity.PersonEntity person = MAPPER.readValue(json, Entity.PersonEntity.class);
+				
+				// Check if we support the language.
+				if (!Stream.of(Languages.values()).anyMatch(l -> l.slug.equals(person.lang))) {
+					LOGGER.warn("Tried to use non-existant language: {}", person.lang);
+					halt(404);
+				}
+				
+				// Make a security token.
+				byte[] tokenBytes = new byte[20];
+				RANDOM.nextBytes(tokenBytes);
+				String token = javax.xml.bind.DatatypeConverter.printHexBinary(tokenBytes);
+				
+				// Insert into db.
+				String sql = "INSERT INTO resumes(id, json, token, ts, template) VALUES(NULL, ?, ?, NOW(), ?)";
+				PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+				stmt.setString(1, json);
+				stmt.setString(2, token);
+				stmt.setString(3, req.queryParams("templateslug"));
+				stmt.executeUpdate();
+				
+				// Get the db generated id.
+				ResultSet generated = stmt.getGeneratedKeys();
+				generated.next();
+				long id = generated.getLong(1);
+				
+				conn.commit();
+				
+				// Redirect to the actual generator.
+				res.redirect("/resume?id=" + id + "&token=" + token);
+				return null;
+			}
+		});
+		
+		get("/raw", (req, res) -> {
+			String template = editorPages.get(Editor.RAW);
+			Context ctx = new Context();
+			ctx.setVariable("templates", Template.values());
+			ctx.setVariable("defaultResume", defaultResume);
+			return THYMELEAF.process(template, ctx);
+		});
+		
+		get("/", (req, res) -> {
+			String template = editorPages.get(Editor.ANGULARJS1X);
+			Context ctx = new Context();
+			ctx.setVariable("templates", Template.values());
+			ctx.setVariable("languages", Languages.values());
+			ctx.setVariable("defaultResume", defaultResume);
+			return THYMELEAF.process(template, ctx);
+		});
+		
+		get("/resume", (req, res) -> {
+			long id = Long.parseLong(req.queryParams("id"));
+			String token = req.queryParams("token");
+			
+			try(Connection conn = DriverManager.getConnection("jdbc:h2:mem:resume;DB_CLOSE_DELAY=-1", "sa", "")) {
+				// Delete all records older than 10 minutes for privacy and so the db doesn't get too large.
+				conn.createStatement().executeUpdate("DELETE FROM resumes WHERE DATEDIFF('MINUTE', ts, NOW()) > 10");
+				conn.commit();
+				
+				String sql = "SELECT token, json, template FROM resumes WHERE id = ?";
+				
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				stmt.setLong(1, id);
+				ResultSet rs = stmt.executeQuery();
+				rs.next();
+
+				String tokenCompare = rs.getString(1);
+				if (!MessageDigest.isEqual(token.getBytes("UTF-8"), tokenCompare.getBytes("UTF-8"))) {
+					LOGGER.warn("Tried to access resume with incorrect token. Resume ID: {}", id);
+					halt(403);
+				}
+				
+				String json = rs.getString(2);
+				Entity.PersonEntity person = MAPPER.readValue(json, Entity.PersonEntity.class);
+				Entity.LangEntity lang = MAPPER.readValue(langs.get(person.lang), Entity.LangEntity.class);
+				String templateSlug = rs.getString(3);
+				String template = templates.get(templateSlug);
+				
+				LOGGER.info("Outputting resume PDF now. ID: {}, LANG: {}, TEMPLATE: {}", id, person.lang, templateSlug);
+				
+				Context ctx = new Context();
+				ctx.setVariable("person", person.person);
+				ctx.setVariable("lang", lang);
+				String html = THYMELEAF.process(template, ctx);
+								
+				ByteArrayOutputStream os = new ByteArrayOutputStream();
+				PdfRendererBuilder builder = new PdfRendererBuilder();
+				builder.withHtmlContent(html, Generator.class.getResource("/").toExternalForm());
+				builder.useUriResolver(new UriResolver());
+				builder.toStream(os);
+				builder.run();
+
+				res.header("Content-Type", "application/pdf");
+				return os.toByteArray();
+			}
+		});
+    }
+}
